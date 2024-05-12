b6e310011075acd2ae55714dde229b10c64c74eb
ZeroDayCoding <zerodaycoding@gmail.com>
2024-05-12 14:59:55 -0500

    Simple hello world program.

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,6 @@
-#include "Bus.hpp"
-#include "Backplane.hpp"
 #include <iostream>
-#include <thread>
-#include <assert.h>
 
 int main(int argc, const char* argv[]) {
-
-    float clockrate = 1 / 1000;
-
-    while(true) {
-        ZDC::Emulator::TickEmulator(clockrate);
-        std::this_thread::yield();
-    }
+    std::cout << "Hello World" << std::endl;
     return 0;
 }
